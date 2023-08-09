956d38e1496de8cc9371f2d1cd7e5d506fcef92d
thaisaraujosantos <thaisaraujo.dev@gmail.com>
2023-08-09 16:00:57 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import HeroSection from "./assets/components/Hero"
+import Navigation from "./assets/components/Navigation"
+
+export default function App() {
+  return(
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  ) 
+}
